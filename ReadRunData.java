0ed6b24244e96cb8214f5acbc1a65452d71456eb
petesndrs <petesndrs@users.noreply.github.com>
2025-04-13 15:48:37 +0100

    Add a main

---
+++ ReadRunData.java
@@ -0,0 +1,12 @@
+class ReadRunData
+{
+  private void run(){
+    System.out.printf("Run");
+  }
+
+  public static void main(String args[])
+  {
+    ReadRunData readRunData = new ReadRunData();
+    readRunData.run();
+  }
+}
