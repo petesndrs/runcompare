07835ac0fa1303f85230a7161ab32138ae6ab99d
petesndrs <petesndrs@users.noreply.github.com>
2025-04-13 17:49:00 +0100

    Download page and instantiate a parser

---
+++ RunPageParser.java
@@ -0,0 +1,15 @@
+import javax.swing.text.html.*;
+
+public class RunPageParser extends HTMLEditorKit
+{
+
+  /**
+   * Call to obtain a HTMLEditorKit.Parser object.
+   * 
+   * @return A new HTMLEditorKit.Parser object.
+   */
+  public HTMLEditorKit.Parser getParser()
+  {
+    return super.getParser();
+  }
+}
---
+++ execute.cmd
@@ -0,0 +1,5 @@
+
+"c:\Program Files\Java\jdk-24\bin\javac" ReadRunData.java
+"c:\Program Files\Java\jdk-24\bin\javac" RunPageParser.java
+
+"c:\Program Files\Java\jdk-24\bin\java" ReadRunData
